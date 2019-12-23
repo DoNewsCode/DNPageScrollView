cb03d80e64ae95f5ca8218aaaf9399710ea6b7b0
李晓越 <lixiaoyue@infinities.com.cn>
2019-12-23 17:41:02 +0800

    解决与右滑返回手势的冲突 2.2.0.1

--- DNPageScrollView/Classes/DNPageCollectionView.m
+++ DNPageScrollView/Classes/DNPageCollectionView.m
@@ -34,5 +34,17 @@
     _gestureBeginHandler = [gestureBeginHandler copy];
 }
 
+/// 12.23 新增，解决与右滑返回手势的冲突
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+    // 首先判断otherGestureRecognizer是不是系统pop手势
+    if ([otherGestureRecognizer.view isKindOfClass:NSClassFromString(@"UILayoutContainerView")]) {
+        // 再判断系统手势的state是began还是fail，同时判断scrollView的位置是不是正好在最左边
+        if (otherGestureRecognizer.state == UIGestureRecognizerStateBegan && self.contentOffset.x == 0) {
+            return YES;
+        }
+    }
+    return NO;
+}
 
 @end
